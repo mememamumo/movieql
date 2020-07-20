d6e4430089de36abfccd0c50e437a84c2f2ac8f4
Yuna Lee <carnivalss@naver.com>
2020-07-21 06:08:41 +0900

    Query and Resolvers

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 
 const server = new GraphQLServer({
+	typeDefs: "graphql/schema.graphql",
+	resolvers
+});
 
-})
-
-server.start(() => console.log("😎Graphql Server Running"))
+server.start(() => console.log("😎Graphql Server Running on http://localhost:4000"));
